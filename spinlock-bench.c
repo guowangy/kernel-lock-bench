c10daf00ccae8e054e4c1690b22c7719191d494c
Wangyang Guo <wangyang.guo@intel.com>
2025-06-09 11:12:32 +0800

    add thread worker function

--- spinlock-bench.c
+++ spinlock-bench.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/ktime.h>
 
 // critical/non-critical section design comes from glibc benchtests:
 // https://github.com/bminor/glibc/blob/glibc-2.40/benchtests/bench-pthread-lock-base.c
@@ -50,16 +51,49 @@ non_critical_section(int length)
 		UNIT_WORK_NON_CRT;
 }
 
+typedef struct worker_params {
+	long iters;
+	int crit_len;
+	int non_crit_len;
+	long duration_ns;
+} worker_params_t;
+
+DEFINE_SPINLOCK(my_spinlock);
+
+static void *worker(void *v)
+{
+	worker_params_t *p = (worker_params_t *)v;
+	long iters = p->iters;
+
+	const ktime_t start_time = ktime_get();
+	while (iters--)
+	{
+		unsigned long flags;
+
+		spin_lock_irqsave(&my_spinlock, flags);
+		critical_section(p->crit_len);
+		spin_unlock_irqrestore(&my_spinlock, flags);
+
+		non_critical_section(p->non_crit_len);
+	}
+	ktime_t end_time = ktime_get();
+	p->duration_ns = ktime_to_ns(ktime_sub(end_time, start_time));
+	return NULL;
+}
+
 // Initialization function (called when the module is loaded)
-static int __init spinlock_bench_init(void) {
+static int __init spinlock_bench_init(void)
+{
 	printk(KERN_INFO "spinlock bench loaded\n");
 	return 0; // Return 0 means success
 }
 
 // Exit function (called when the module is removed)
-static void __exit spinlock_bench_exit(void) {
+static void __exit spinlock_bench_exit(void)
+{
 	printk(KERN_INFO "spinlock bench unloaded\n");
 }
+
 
 // Register the functions
 module_init(spinlock_bench_init);
